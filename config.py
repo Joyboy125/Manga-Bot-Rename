7893136e2a56bf0731c7c4f4d9f37e17148b6031
joy boy <156354499+Joyboy125@users.noreply.github.com>
2024-10-14 21:19:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
+import os
+
 env_vars = {
   # Get From my.telegram.org
   "API_HASH": "f757ca005f4a320bcea5ced947bbff5e",
   # Get From my.telegram.org
   "API_ID": "27011028",
   #Get For @BotFather
-  "BOT_TOKEN": "7505997121:AAFgGywTfb2-BYIqaRUPTd2W4RJNIn7bHK4",
+  
+  # zoro bot = 7505997121:AAFgGywTfb2-BYIqaRUPTd2W4RJNIn7bHK4
+  "BOT_TOKEN": "7250357906:AAEXZ9nIHLmASZ909wJLCBwadBCiO9xTRD8", # joy boy bot = 7250357906:AAEXZ9nIHLmASZ909wJLCBwadBCiO9xTRD8 
+
   # Get For tembo.io
   "DATABASE_URL_PRIMARY": "postgresql://postgres:k0edUULYD0G6vZy0@fruitfully-legitimate-lemur.data-1.use1.tembo.io:5432/postgres",
   # Logs Channel Username Without @
@@ -16,6 +21,15 @@ env_vars = {
   # Ex : Chapter {chap_num} {chap_name} @Manhwa_Arena
   "FNAME": "{chap_num}. {chap_name}"
 }
+
+username = "mojawatjay26"
+password = "DMSZJakoTKDqZyYD"
+db_name = "updatebot"
+collection_name = "Channels"
+USER = 1355560957
+
+
+DB_URL  = os.environ.get("DB_URL","mongodb+srv://mojawatjay26:DMSZJakoTKDqZyYD@updatebot.qk2u8.mongodb.net/?retryWrites=true&w=majority&appName=updatebot")
 
 dbname = env_vars.get('DATABASE_URL_PRIMARY') or env_vars.get('DATABASE_URL') or 'sqlite:///test.db'
 
